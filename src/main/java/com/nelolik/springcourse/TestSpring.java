b03e1ecaa9f6fe7bf416c700caa29a61770076ff
nelolik <parshikovav@gmail.com>
2022-01-05 19:33:54 +0300

    Different scopes.

--- src/main/java/com/nelolik/springcourse/TestSpring.java
+++ src/main/java/com/nelolik/springcourse/TestSpring.java
@@ -9,10 +9,22 @@ public class TestSpring {
         );
 
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume()) ;
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comp = firstMusicPlayer == secondMusicPlayer;
+        System.out.println(comp);
+
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+
+//        musicPlayer.playMusic();
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume()) ;
 
         context.close();
     }
