660976bda032df40182875e8eab5f9ade25819bb
nelolik <parshikovav@gmail.com>
2022-01-08 15:55:58 +0300

    Data transmission from a controller to a view using thymeleaf

--- SecondProject/src/main/java/com/nelolik/springcourse/controllers/FirsController.java
+++ SecondProject/src/main/java/com/nelolik/springcourse/controllers/FirsController.java
@@ -1,6 +1,7 @@
 package com.nelolik.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,13 +13,11 @@ import javax.servlet.http.HttpServletRequest;
 public class FirsController  {
 
     @GetMapping("/hello")
-//    public String helloPage(HttpServletRequest request) {
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-//        String name = request.getParameter("name");
-//        String surname = request.getParameter("surname");
-
-        System.out.println("Hello " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+//        System.out.println("Hello " + name + " " + surname);
+        model.addAttribute("message", "Hello " + name + " " + surname);
 
         return "first/hello";
     }
