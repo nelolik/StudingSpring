28f4bd8032ee82e0393a48c32f3f0551982267d0
nelolik <parshikovav@gmail.com>
2022-01-08 15:43:52 +0300

    Processing request parameters.

--- SecondProject/src/main/java/com/nelolik/springcourse/controllers/FirsController.java
+++ SecondProject/src/main/java/com/nelolik/springcourse/controllers/FirsController.java
@@ -3,13 +3,23 @@ package com.nelolik.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirsController  {
 
     @GetMapping("/hello")
-    public String helloPage() {
+//    public String helloPage(HttpServletRequest request) {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+
+        System.out.println("Hello " + name + " " + surname);
+
         return "first/hello";
     }
 
