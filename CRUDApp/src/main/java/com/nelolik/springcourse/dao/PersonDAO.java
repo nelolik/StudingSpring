dc55a9313caae60084a363b04ec1aef9c4de86b2
nelolik <parshikovav@gmail.com>
2022-01-08 22:14:01 +0300

    Added editing, removing and adding people.

--- CRUDApp/src/main/java/com/nelolik/springcourse/dao/PersonDAO.java
+++ CRUDApp/src/main/java/com/nelolik/springcourse/dao/PersonDAO.java
@@ -31,4 +31,13 @@ public class PersonDAO {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
     }
+
+    public void update(int id, Person person) {
+        Person personToBeUpdated = show(id);
+        personToBeUpdated.setName(person.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(person -> person.getId() == id);
+    }
 }
